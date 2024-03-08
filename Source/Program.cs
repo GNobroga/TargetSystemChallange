94b7b435bcc358d834a25f877305563b4b600644
Gabiroba <gabrielcardosogirarde@gmail.com>
2024-03-08 04:14:25 -0300

    chore: README.md

--- Source/Program.cs
+++ Source/Program.cs
@@ -1,15 +1,16 @@
 ﻿
 // Question 1
 // Valor da soma 91
+using System.Text;
+
 Console.WriteLine("SOMA: " + Enumerable.Range(0, 14).Aggregate(0, (initial, current) => initial + current));
 Console.WriteLine();
 
 // Question 2
 static IEnumerable<int> FibonnaciSequence(int number) {
-    if (number <= 0) throw new ArgumentException("O número não pode ser menor que zero");
+    if (number <= 0) throw new ArgumentException("O número não pode ser menor ou igual a zero");
     for (int previous = -1, future = 1, term = 0, i = 1; i <= number; previous = future, future = term, i += 1) {
         term = previous + future;
-        Console.Write($" {term} ");
         yield return term;
     } 
 }
@@ -25,13 +26,10 @@ else
 // Question 5
 var phrase = "GABRIEL QUER UM PLAYSTATION";
 
-static string Reverse(string phrase, int size) 
+static StringBuilder Reverse(string phrase, int size) 
 {
-    if (size == 0) return "";
-    var concat = Reverse(phrase, size - 1);
-    concat += phrase[^size];
-    return concat;
+    if (size <= 0) return new StringBuilder();
+    return Reverse(phrase, size - 1).Append(phrase[^size]);
 }
 
-Console.WriteLine();
-Console.WriteLine("REVERSO: " + Reverse(phrase, phrase.Length));
+Console.WriteLine("REVERSO: " + Reverse(phrase, phrase.Length).ToString());
